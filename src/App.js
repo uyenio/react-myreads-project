f2c6c650b7ac1debb299cfd8bd74db082c98650d
Uyen <contact@uyen.io>
2018-04-17 15:11:37 +0700

    Add routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Bookshelf from './Bookshelf';
+import { Link, Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
@@ -11,7 +12,6 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false,
     books: [],
     foundBooks: [],
     searchBookResults: [],
@@ -51,64 +51,64 @@ class BooksApp extends React.Component {
   }
 
   closeSearch = () => {
-    this.setState({ showSearchPage: false})
     this.setState({ searchBookResults: [] })
   }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={this.closeSearch}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+      <Route path='/search' render={() => (
+        <div className="search-books">
+        <div className="search-books-bar">
+          <Link to='/' className="close-search" onClick={this.closeSearch}>Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" 
-                value={this.query} onChange={(event) => this.search(event.target.value)}/>
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid">
-                {this.state.searchBookResults.map((book) => (
-                  <li key={book.id}>
-                      <div className="book">
-                      <div className="book-top">
-                          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-                          <div className="book-shelf-changer">
-                          <select value={book.shelf} onChange={(event) => this.moveBook(book, event.target.value)} >
-                            <option value="none" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                          </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                      <div>Bookshelf: {book.shelf}</div>
-                      </div>
-                  </li>
-                ))}
-              </ol>
-            </div>
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input type="text" placeholder="Search by title or author" 
+            value={this.query} onChange={(event) => this.search(event.target.value)}/>
           </div>
-        ) : (
-          <div>
-            <Bookshelf books={this.state.books} moveBook={this.moveBook}/>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-         </div>
-        )}
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.searchBookResults.map((book) => (
+              <li key={book.id}>
+                  <div className="book">
+                  <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+                      <div className="book-shelf-changer">
+                      <select value={book.shelf} onChange={(event) => this.moveBook(book, event.target.value)} >
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                      </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{book.authors}</div>
+                  <div>Bookshelf: {book.shelf}</div>
+                  </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+      )} />
+      <Route exact path='/' render={() => (
+        <div>
+          <Bookshelf books={this.state.books} moveBook={this.moveBook}/>
+          <div className="open-search">
+            <Link to='/search'>Add a book</Link>
+          </div>
+       </div>
+      )}/>
       </div>
       
     )
