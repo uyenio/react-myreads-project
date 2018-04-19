d2ca5742036babb5a0f8b477742f6ef355b6c8fb
Uyen <contact@uyen.io>
2018-04-19 11:41:14 +0700

    fix: check for thumbnail and author in search

--- src/Book.js
+++ src/Book.js
@@ -6,11 +6,11 @@ const Book = ({book, moveBook}) => {
     return (
         <div className="book">
         <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: typeof(book.imageLinks) !== 'undefined' ? `url('${book.imageLinks.thumbnail}')` : 'none'}}></div>
             <BookshelfChanger book={ book } moveBook={ moveBook } />
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
+        <div className="book-authors">{typeof(book.authors) !== 'undefined' ? book.authors : 'Not Available'}</div>
         </div>
     );
 };
