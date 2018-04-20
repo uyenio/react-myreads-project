269bf6821a61edca664d47841d566371621fcb2d
Uyen <contact@uyen.io>
2018-04-20 22:29:40 +0700

    refactor: use style variable

--- src/Book.js
+++ src/Book.js
@@ -3,10 +3,16 @@ import PropTypes from 'prop-types';
 import BookshelfChanger from './BookshelfChanger'
 
 const Book = ({book, moveBook}) => { 
+    const style = { 
+        width: 128, 
+        height: 193, 
+        backgroundImage: typeof(book.imageLinks) !== 'undefined' ? `url('${book.imageLinks.thumbnail}')` : 'none'
+    };
+    
     return (
         <div className="book">
         <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: typeof(book.imageLinks) !== 'undefined' ? `url('${book.imageLinks.thumbnail}')` : 'none'}}></div>
+            <div className="book-cover" style={style}></div>
             <BookshelfChanger book={ book } moveBook={ moveBook } />
         </div>
         <div className="book-title">{book.title}</div>
